0518f118236ec6989e68bc8b65c8ed848fedd275
Cary-Walsh <walsh.cary.a@gmail.com>
2020-03-14 13:02:27 -0400

    Initial ViewModel implementation

--- app/src/main/java/com/example/threehourweather/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/threehourweather/viewmodel/WeatherViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.threehourweather.viewmodel
 
-class WeatherViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.threehourweather.model.WeatherResponse
+
+class WeatherViewModel(val baseUrl: String) : ViewModel() {
+    private val currentDataSet: MutableLiveData<WeatherResponse> = MutableLiveData()
+    private val forecastDataSet: MutableLiveData<WeatherResponse> = MutableLiveData()
+
+    fun getCurrentDataSet(): LiveData<WeatherResponse> {
+        return currentDataSet
+    }
+
+    fun getForecastDataSet(): LiveData<WeatherResponse> {
+        return forecastDataSet
+    }
+
+    fun getCurrentWeatherData(dataSet: WeatherResponse) {
+        this.currentDataSet.value = dataSet
+    }
+
+    fun getForecastWeatherData(dataSet: WeatherResponse) {
+        this.forecastDataSet.value = dataSet
+    }
 }
